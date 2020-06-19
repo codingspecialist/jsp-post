007204e305b37fc3f0dde52bf7965cbdf2899680
ssar <ssar@nate.com>
2020-06-19 12:39:13 +0900

    11. 글목록보기

--- src/com/cos/apple/dao/PostDao.java
+++ src/com/cos/apple/dao/PostDao.java
@@ -3,7 +3,11 @@ package com.cos.apple.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.cos.apple.db.DBConn;
+import com.cos.apple.model.Post;
 
 
 public class PostDao {
@@ -12,6 +16,31 @@ public class PostDao {
 	private PreparedStatement pstmt;
 	private ResultSet rs;
 
+	public List<Post> 글목록() {
+		try {
+			String sql = "SELECT * FROM post ORDER BY id DESC";
+			conn = DBConn.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			List<Post> posts = new ArrayList<>();
+			while(rs.next()) {
+				Post post = new Post();
+				post.setId(rs.getInt("id"));
+				post.setMemberId(rs.getInt("memberId"));
+				post.setTitle(rs.getString("title"));
+				post.setContent(rs.getString("content"));
+				post.setCreateDate(rs.getTimestamp("createDate"));
+				posts.add(post);
+			}
+			return posts;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	public int 글쓰기(int memberId, String title, String content) {
 		try {
 			String sql = "INSERT INTO post VALUES(post_seq.nextval, ?,?,?, sysdate)";
